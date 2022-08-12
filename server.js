200a05b80d88a755b8c731b04df33f7d7a7daa4a
Abdurahim <nurmatov.abdurahim98@mail.ru>
2022-08-12 12:12:40 +0500

    implement cors

--- server.js
+++ server.js
@@ -25,3 +25,10 @@ process.on('unhandledRejection', (err) => {
   console.log('UNHANDLED REJECTION! * Shutting down...');
   server.close(() => process.exit(1));
 });
+
+process.on('SIGTERM', () => {
+  console.log('!!! SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('Process terminated!');
+  });
+});
